c2769ffe63cc25309fed654abc7feafd71a279f1
aarora8 <ashish.arora.88888@gmail.com>
2021-08-24 19:58:46 -0400

    minor fix

--- egs/safet/ASR/prepare.py
+++ egs/safet/ASR/prepare.py
@@ -12,14 +12,14 @@ from collections import defaultdict
 
 import torch
 import lhotse
-from lhotse.utils import fastcopy
 from lhotse import CutSet, Fbank, FbankConfig, LilcomHdf5Writer, combine
-from lhotse.recipes import prepare_ami, prepare_musan
+from lhotse import load_manifest
+from lhotse.recipes import prepare_safet, prepare_musan
+from lhotse.utils import fastcopy
 from lhotse import validate_recordings_and_supervisions
 from lhotse.audio import Recording, RecordingSet
 from lhotse.supervision import SupervisionSegment, SupervisionSet
 from lhotse.utils import Pathlike, check_and_rglob, recursion_limit
-
 from snowfall.common import str2bool
 
 # Torch's multithreaded behavior needs to be disabled or it wastes a lot of CPU and
@@ -74,45 +74,18 @@ def get_parser():
         '--num-jobs',
         type=int,
         default=min(15, os.cpu_count()),
-        help='When enabled, use 960h LibriSpeech.')
+        help='number if cpu jobs')
     return parser
 
 
 def main():
     args = get_parser().parse_args()
-
+    output_dir = Path('exp/data')
     musan_dir = locate_corpus(
         Path('/export/corpora5/JHU/musan'),
         Path('/export/common/data/corpora/MUSAN/musan'),
         Path('/root/fangjun/data/musan'),
     )
-
-    output_dir = Path('exp/data')
-    print('ami manifest preparation:')
-    ami_manifests = defaultdict(dict)
-    recording_set_dev, supervision_set_dev = lhotse.kaldi.load_kaldi_data_dir('/exp/aarora/archive/snowfall/ami/kaldi_data/dev', 16000)
-    validate_recordings_and_supervisions(recording_set_dev, supervision_set_dev)
-    supervision_set_dev.to_json(output_dir / f'supervisions_dev.json')
-    ami_manifests['dev'] = {
-                'recordings': recording_set_dev,
-                'supervisions': supervision_set_dev
-            }
-
-    recording_set_eval, supervision_set_eval = lhotse.kaldi.load_kaldi_data_dir('/exp/aarora/archive/snowfall/ami/kaldi_data/eval', 16000)
-    validate_recordings_and_supervisions(recording_set_eval, supervision_set_eval)
-    supervision_set_eval.to_json(output_dir / f'supervisions_eval.json')
-    ami_manifests['eval'] = {
-                'recordings': recording_set_eval,
-                'supervisions': supervision_set_eval
-            }
-
-    recording_set_train, supervision_set_train = lhotse.kaldi.load_kaldi_data_dir('/exp/aarora/archive/snowfall/ami/kaldi_data/train', 16000)
-    validate_recordings_and_supervisions(recording_set_train, supervision_set_train)
-    supervision_set_eval.to_json(output_dir / f'supervisions_train.json')
-    ami_manifests['train'] = {
-                'recordings': recording_set_train,
-                'supervisions': supervision_set_train
-            }
 
     print('Musan manifest preparation:')
     musan_cuts_path = output_dir / 'cuts_musan.json.gz'
@@ -122,34 +95,80 @@ def main():
         parts=('music', 'speech', 'noise')
     )
 
+    #print('safet manifest preparation:')
+    #safet_manifests = prepare_safet(
+    #    corpus_dir='/exp/aarora/corpora/safet/',
+    #    lexicon_dir='data/local/dict_nosp/lexicon/',
+    #    output_dir=output_dir
+    #)
+
+    print('safet manifest preparation:')
+    safet_manifests = defaultdict(dict)
+    recording_set_dev_clean, supervision_set_dev_clean = lhotse.kaldi.load_kaldi_data_dir('/home/hltcoe/aarora/kaldi/egs/opensat20/s5/data/dev_clean', 16000)
+    validate_recordings_and_supervisions(recording_set_dev_clean, supervision_set_dev_clean)
+    supervision_set_dev_clean.to_json(output_dir / f'supervisions_safet_dev_clean.json')
+    safet_manifests['dev_clean'] = {
+                'recordings': recording_set_dev_clean,
+                'supervisions': supervision_set_dev_clean
+            }
+
+    recording_set_dev, supervision_set_dev = lhotse.kaldi.load_kaldi_data_dir('/home/hltcoe/aarora/kaldi/egs/opensat20/s5/data/safe_t_dev1', 16000)
+    validate_recordings_and_supervisions(recording_set_dev, supervision_set_dev)
+    supervision_set_dev.to_json(output_dir / f'supervisions_safet_dev.json')
+    safet_manifests['dev'] = {
+                'recordings': recording_set_dev,
+                'supervisions': supervision_set_dev
+            }
+
+    recording_set_train, supervision_set_train = lhotse.kaldi.load_kaldi_data_dir('/home/hltcoe/aarora/kaldi/egs/opensat20/s5/data/train_cleaned', 16000)
+    validate_recordings_and_supervisions(recording_set_train, supervision_set_train)
+    supervision_set_train.to_json(output_dir / f'supervisions_safet_train.json')
+    safet_manifests['train'] = {
+                'recordings': recording_set_train,
+                'supervisions': supervision_set_train
+            }
+
+    sups = load_manifest('exp/data/supervisions_safet_train.json')
+    f = open('exp/data/lm_train_text', 'w')
+    for s in sups:
+        print(s.text, file=f)
+
+    sups = load_manifest('exp/data/supervisions_safet_dev_clean.json')
+    f = open('exp/data/lm_dev_text', 'w')
+    for s in sups:
+        print(s.text, file=f)
+
     print('Feature extraction:')
     extractor = Fbank(FbankConfig(num_mel_bins=80))
     with get_executor() as ex:  # Initialize the executor only once.
-        for partition, manifests in ami_manifests.items():
-            print(f"Processing {partition} ")
-            if (output_dir / f'cuts_ami_{partition}.json.gz').is_file():
+        for partition, manifests in safet_manifests.items():
+            if (output_dir / f'cuts_safet_{partition}.json.gz').is_file():
                 print(f'{partition} already exists - skipping.')
                 continue
+            print('Processing', partition)
             cut_set = CutSet.from_manifests(
                 recordings=manifests['recordings'],
                 supervisions=manifests['supervisions']
             )
             cut_set = cut_set.trim_to_supervisions()
             cut_set = cut_set.map(lambda c: fastcopy(c, supervisions=[s for s in c.supervisions if s.start == 0 and abs(s.duration - c.duration) <= 1e-3]))
+            if partition != 'train':
+                print(f'filtering cuts in {partition} partition.')
+                cut_set = cut_set.filter(lambda c: c.duration >= 1)
             if 'train' in partition:
+                #cut_set.to_json(output_dir / f'cuts_safet__wo_sp_{partition}.json.gz')
                 cut_set = cut_set + cut_set.perturb_speed(0.9) + cut_set.perturb_speed(1.1)
 
-            print(f"store cutset supervision")
             cut_set = cut_set.compute_and_store_features(
                 extractor=extractor,
-                storage_path=f'{output_dir}/feats_ami_{partition}',
-                # when an executor is specified, make more partitions
+                storage_path=f'{output_dir}/feats_safet_{partition}',
                 num_jobs=args.num_jobs if ex is None else 80,
                 executor=ex,
                 storage_type=LilcomHdf5Writer
             )
-            ami_manifests[partition]['cuts'] = cut_set
-            cut_set.to_json(output_dir / f'cuts_ami_{partition}.json.gz')
+            safet_manifests[partition]['cuts'] = cut_set
+            cut_set.to_json(output_dir / f'cuts_safet_{partition}.json.gz')
+
         # Now onto Musan
         if not musan_cuts_path.is_file():
             print('Extracting features for Musan')
@@ -168,3 +187,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+
