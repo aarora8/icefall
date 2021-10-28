d7879d5a120f64744ccd06d48050e53702d33cda
aarora8 <ashish.arora.88888@gmail.com>
2021-10-28 19:54:45 -0400

    minor fix

--- egs/chime6/ASR/local/prepare.py
+++ egs/chime6/ASR/local/prepare.py
@@ -43,8 +43,12 @@ def main():
     cut_set = CutSet.from_manifests(recordings=recording_set_eval, supervisions=supervision_set_eval,
                                         features=feature_set_eval).trim_to_supervisions()
 
-    chime_manifests['eval']['cuts'] = cut_set
-    cut_set.to_json(output_dir / f'cuts_chime_eval_gss.json.gz')
+    #chime_manifests['eval']['cuts'] = cut_set
+    chime_manifests['eval_gss'] = {
+                'recordings': recording_set_eval,
+                'supervisions': supervision_set_eval
+            }
+    cut_set.to_json(output_dir / f'cuts_chime_eval_gss_1.json.gz')
     supervision_set_eval.to_json(output_dir / f'supervisions_chime_eval_gss.json')
     recording_set_eval.to_json(output_dir / f'recordings_chime_eval_gss.json')
 
@@ -56,8 +60,12 @@ def main():
     validate_recordings_and_supervisions(recording_set_dev, supervision_set_dev)
     cut_set = CutSet.from_manifests(recordings=recording_set_dev, supervisions=supervision_set_dev,
                                         features=feature_set_dev).trim_to_supervisions()
-    chime_manifests['dev']['cuts'] = cut_set
-    cut_set.to_json(output_dir / f'cuts_chime_dev_gss.json.gz')
+    #chime_manifests['dev']['cuts'] = cut_set
+    chime_manifests['dev_gss'] = {
+                'recordings': recording_set_dev,
+                'supervisions': supervision_set_dev
+            }
+    cut_set.to_json(output_dir / f'cuts_chime_dev_gss_1.json.gz')
     supervision_set_dev.to_json(output_dir / f'supervisions_chime_dev_gss.json')
     recording_set_dev.to_json(output_dir / f'recordings_chime_dev_gss.json')
 
@@ -69,8 +77,12 @@ def main():
     validate_recordings_and_supervisions(recording_set_train, supervision_set_train)
     cut_set = CutSet.from_manifests(recordings=recording_set_train, supervisions=supervision_set_train,
                                         features=feature_set_train).trim_to_supervisions()
-    chime_manifests['train']['cuts'] = cut_set
-    cut_set.to_json(output_dir / f'cuts_chime_train.json.gz')
+    #chime_manifests['train']['cuts'] = cut_set
+    chime_manifests['train'] = {
+                'recordings': recording_set_train,
+                'supervisions': supervision_set_train
+            }
+    cut_set.to_json(output_dir / f'cuts_chime_train_1.json.gz')
     supervision_set_train.to_json(output_dir / f'supervisions_chime_train.json')
     recording_set_train.to_json(output_dir / f'recordings_chime_train.json')
 
