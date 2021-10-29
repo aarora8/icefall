7dc56a7c30c6f4d854d891ca53dba7b8cd7b0b8a
aarora8 <ashish.arora.88888@gmail.com>
2021-10-29 19:43:21 -0400

    running k2

--- egs/librispeech/ASR/utils/activate_k2_release.sh
+++ egs/librispeech/ASR/utils/activate_k2_release.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-K2_ROOT=/exp/aarora/icefall_work_env/k2/
+K2_ROOT=/exp/aarora/icefall_work_env/k2_me/
 export PYTHONPATH=$K2_ROOT/k2/python:$PYTHONPATH
 export PYTHONPATH=$K2_ROOT/build_release/lib:$PYTHONPATH
