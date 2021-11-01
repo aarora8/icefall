73ce9817ddcaf2ea3f725a4b8debcd562ef5d8d0
aarora8 <ashish.arora.88888@gmail.com>
2021-10-31 22:54:32 -0400

    running with a particular gpu

--- egs/chime6/ASR/prepare.sh
+++ egs/chime6/ASR/prepare.sh
@@ -140,4 +140,4 @@ if [ $stage -le 9 ] && [ $stop_stage -ge 9 ]; then
   done
 fi
 
-#local/queue.pl --mem 30G --config local/coe.conf data/train.log ~/miniconda3/envs/k2_scratch2/bin/python3 ./tdnn_lstm_ctc/train.py
+# local/queue.pl --mem 30G --gpu 1 --config local/coe.conf data/train.log ~/miniconda3/envs/k2_scratch2/bin/python3 ./tdnn_lstm_ctc/train.py
